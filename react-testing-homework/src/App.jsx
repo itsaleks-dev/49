ff19ce92193d6abd4ef5a1d0b51f3819b023fee1
Laptop <admin@MacBook-Pro.local>
2025-10-31 21:42:09 +0200

    added settings app.jsx and main.jsx

---
+++ react-testing-homework/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { lightTheme, darkTheme } from "./styles/theme";
+import { useTheme } from "./hooks/useTheme";
+
+import UserProfile from "./features/user/UserProfile";
+import ContactForm from "./features/contact/ContactForm";
+
+import ErrorBoundary from "./components/ErrorBoundary";
+
+export default function App() {
+    const { themeName, toggleName } = useTheme();
+    const theme = themeName === "dark" ? darkTheme : lightTheme;
+
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <button onClick={toggleTheme}>Toggle Theme</button>
+
+            <ErrorBoundary>
+                <UserProfile />
+            </ErrorBoundary>
+
+            <ContactForm />
+        </ThemeProvider>
+    );
+}
