709021c0e4a27cb0ce8afd2a67efd905f45f5d54
Laptop <admin@MacBook-Pro.local>
2025-10-31 20:03:26 +0200

    create theme.js file and added styles

---
+++ react-testing-homework/src/styles/theme.js
@@ -0,0 +1,19 @@
+export const lightTheme = {
+    name: "light",
+    bg: "#ffffff",
+    text: "#222",
+    card: "#f9f9f9",
+    border: "#ddd",
+    primary: "#007bff",
+    shadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
+};
+
+export const darkTheme = {
+    name: "dark",
+    bg: "#121212",
+    text: "#f1f1f1",
+    card: "#1e1e1e",
+    border: "#333",
+    primary: "#3498db",
+    shadow: "0 4px 12px rgba(255, 255, 255, 0.1)",
+};
